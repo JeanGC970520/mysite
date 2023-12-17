19f73418b2c41462a7a0bacd739e189ea8edbcd1
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-12-17 17:28:02 -0600

    Creating methods to call when an end-point been calling

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+# Funciones que se ejecutaran cuando un end-point sea llamado. 
+def hello(request):
+    return HttpResponse("<h1>Hello world</h1>")
 
+def about(request):
+    return HttpResponse("About")
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from myapp import views
+
+# Router de los end-points
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.hello),
+    path("about/", views.about),
 ]
