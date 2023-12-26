6bbd16f0cb1ff729d6c573038a52e3dc2c7c81e2
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-12-26 13:11:04 -0600

    Creating models. Learning how Django handle this models

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    # ! Relacionar tablas.
+    # ! Para relacionar las tablas usamos un ForeingKey y en este caso cuando se elimine un 
+    # ! un Project, todo lo relacionado con el queremos que tambien se elimine, por lo cual
+    # ! usamos el param on_delete en modo CASCADE.
+    project = models.ForeignKey(Project, on_delete=models.CASCADE) 
