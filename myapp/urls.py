ae4c8c5d870d42262f089765ae39092ceadd4c70
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-12-27 13:38:35 -0600

    Learning to handle URL param

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 # Router de los end-points
 urlpatterns = [
-    path("", views.hello),
+    path("", views.index),
     path("about/", views.about),
+    # ! IMPORTANTE: Definicion de un URL param. 
+    # ! El cual es de tipo str y se llamara 'username', el metodo hello() del modulo views
+    # ! tendra que recibir dicho parametro y hacer la operacion correspondiente.
+    path("hello/<str:username>", views.hello),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -4,8 +4,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 # Funciones que se ejecutaran cuando un end-point sea llamado. 
-def hello(request):
-    return HttpResponse("<h1>Hello world</h1>")
+def index(request):
+    return HttpResponse("Index page")
+
+# ! Metodo que recibe un URL param llamado 'username'
+def hello(request, username):
+    print(f"Username: {username} of type {type(username)}")
+    return HttpResponse(f"<h1>Hello {username}</h1>")
 
 def about(request):
     return HttpResponse("About")
